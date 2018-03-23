c5b7d49cbfa96df36ddb32eed8a3b2999d02b0c1
1339521171 <37705817+1339521171@users.noreply.github.com>
2018-03-24 00:51:17 +0800

    框架
    框架

---
+++ application/home/controller/VoiceAi.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: likang
+ * Date: 2018/3/14
+ * Time: 14:33
+ */
+
+namespace app\home\controller;
+
+
+class VoiceAi
+{
+
+
+    public function receiveFile(){
+
+        return "接收文件";
+
+    }
+
+
+}
